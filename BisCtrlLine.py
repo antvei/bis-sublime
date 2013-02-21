965fd998ce980e80356c4457071b8a4729416dbe
Ed Winn <wickchucked@gmail.com>
2013-02-21 13:29:24 -0600

    ignore .cache updated packages.json

--- BisCtrlLine.py
+++ BisCtrlLine.py
@@ -21,7 +21,8 @@ class BisExecCommand(sublime_plugin.TextCommand):
         directory = os.path.dirname(path)
         file_name = os.path.basename(path)
         directory = directory + "\BisCmd.bis"
-        output    = file_name + "|" + text
+        output    = path + "|" + file_name + "|" + text
+        current_user = os.environ.get("USERNAME")
 
         #  Check if BisCmd script is registered
         if os.path.exists("C:\Unisys\Clients\MPC\Scripts\BisCmd.ATR"):
@@ -39,7 +40,7 @@ class BisExecCommand(sublime_plugin.TextCommand):
             })
 
         #  Check if BisCmd script is registered
-        if os.path.exists("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR"):
+        elif os.path.exists("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR"):
 
             # shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.ATR")
             # shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.SCR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.SCR")
@@ -53,33 +54,33 @@ class BisExecCommand(sublime_plugin.TextCommand):
               "shell": True
             })
 
-        # else:
-        #     print "Could not find BisExec script in current directory"
-        #     print "Now creating it..."
+        else:
+            print "Could not find BisExec script in current directory"
+            # print "Now creating it..."
 
-        #     self.view.window().run_command("exec", {
-        #         "cmd": ['C:\Unisys\Clients\MPC$1\GIScriptMaker.exe WINDOWS BisCmd mapperguest E WINNE 14 BisCmd Workstation arthur2 3986 map '' 0 N'],
-        #         "shell": True
-        #     })
+            # self.view.window().run_command("exec", {
+            #     "cmd": ['C:\Unisys\Clients\MPC$1\GIScriptMaker.exe WINDOWS BisCmd mapperguest E WINNE 14 BisCmd Workstation arthur2 3986 map '' 0 N'],
+            #     "shell": True
+            # })
 
-        #     for secs in range(5):
-        #         if os.path.exists("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR"):
+            # for secs in range(5):
+            #     if os.path.exists("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR"):
 
-        #             print "File Exists!"
-        #             shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.ATR")
-        #             shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.SCR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.SCR")
+            #         print "File Exists!"
+            #         shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.ATR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.ATR")
+            #         shutil.copy("C:\Unisys\Clients\MPC$1\Scripts\BisCmd.SCR", "C:\Documents and Settings\winne\Application Data\Unisys\Clients\MPC5.4\Scripts\BisCmd.SCR")
 
-        #             print "Writing BisCmd file"
-        #             with open(directory, "w") as textfile:
-        #                 textfile.write(output)
+            #         print "Writing BisCmd file"
+            #         with open(directory, "w") as textfile:
+            #             textfile.write(output)
 
-        #             self.view.window().run_command("exec", {
-        #               "cmd": ['C:\Unisys\Clients\MPC$1\mpcapi32.exe -cBisCmd -dMAPPER.MSF'],
-        #               "shell": False
-        #             })
-        #             break
+            #         self.view.window().run_command("exec", {
+            #           "cmd": ['C:\Unisys\Clients\MPC$1\mpcapi32.exe -cBisCmd -dMAPPER.MSF'],
+            #           "shell": False
+            #         })
+            #         break
 
-        #         else:
-        #             sleep(1)
-        #             if secs == 4:
-        #                 print "Could not create or find BisCmd.ATR in 5 seconds..."
+            #     else:
+            #         sleep(1)
+            #         if secs == 4:
+            #             print "Could not create or find BisCmd.ATR in 5 seconds..."
